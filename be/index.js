60b503dbd98d9a7b5656a814f65086af8cefa9d1
unknown <syifanurramadhani54@gmail.com>
2025-05-18 17:26:46 +0700

    coba

--- be/index.js
+++ be/index.js
@@ -4,11 +4,12 @@ import NotesRoute from "./routes/NotesRoute.js";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(NotesRoute);
 
-const app = express();
+
 app.set("view engine", "ejs");
 
 app.use(cookieParser());
@@ -20,6 +21,6 @@ app.use(
 );
 app.use(express.json());
 app.get("/", (req, res) => res.render("index"));
-app.use(UserRoute);
+app.use(NotesRoute);
 
 app.listen(5000, () => console.log("Server connected"));
