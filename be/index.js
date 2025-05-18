cb6853e1373bfc557606413eb6ebd516cf0c551b
unknown <syifanurramadhani54@gmail.com>
2025-05-18 17:02:51 +0700

    fixing

--- be/index.js
+++ be/index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import cors from "cors";
 import NotesRoute from "./routes/NotesRoute.js";
-
-const app = express();
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 app.use(cors());
 app.use(express.json());
 app.use(NotesRoute);
 
-app.listen(3000, () => console.log("server terhubung"));
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // <- Diganti sama alamat front-end
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.get("/", (req, res) => res.render("index"));
+app.use(UserRoute);
+
+app.listen(5000, () => console.log("Server connected"));
