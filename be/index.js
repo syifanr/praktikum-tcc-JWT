367f31d5f3dbd28e6018236d2c42136f9a161903
unknown <syifanurramadhani54@gmail.com>
2025-05-20 14:33:07 +0700

    fix: listen on process.env.PORT for Cloud Run

--- be/index.js
+++ be/index.js
@@ -5,22 +5,24 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:5000", 
+  credentials: true,
+}));
+
+app.use(cookieParser());
 app.use(express.json());
+
+
 app.use(NotesRoute);
 
 
 app.set("view engine", "ejs");
 
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5000", // <- Diganti sama alamat front-end
-    credentials: true,
-  })
-);
+
 app.use(express.json());
 app.get("/", (req, res) => res.render("index"));
-app.use(NotesRoute);
+
 
 app.listen(5000, () => console.log("Server connected"));
