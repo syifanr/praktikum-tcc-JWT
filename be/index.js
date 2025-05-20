1e0c04da86e93ab292e9563b64d4fc85b73437f2
unknown <syifanurramadhani54@gmail.com>
2025-05-20 14:42:20 +0700

    fix: listen on process.env.PORT for Cloud Run

--- be/index.js
+++ be/index.js
@@ -25,6 +25,8 @@ app.use(express.json());
 app.get("/", (req, res) => res.render("index"));
 
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
