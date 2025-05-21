a521211e7881dbeb5c271488c10dde6e0b8278ce
unknown <syifanurramadhani54@gmail.com>
2025-05-21 18:20:42 +0700

    ubah cors

--- be/index.js
+++ be/index.js
@@ -14,9 +14,14 @@ const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(cors({
-  origin: "https://notes-syifa194-797713225706.us-central1.run.app", 
+  origin: [
+    "http://localhost:5501", // local dev
+    "https://fe-notessyifa-dot-fit-crow-450802-e0.uc.r.appspot.com", // FE deploy
+    "https://notes-syifa194-797713225706.us-central1.run.app", // backend sendiri 
+  ],
   credentials: true,
 }));
+
 
 app.use(cookieParser());
 app.use(express.json());
