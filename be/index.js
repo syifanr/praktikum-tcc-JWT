c405a8f5f984b4ede55d36bcf89a050cfac0af5e
unknown <syifanurramadhani54@gmail.com>
2025-05-22 16:35:46 +0700

    bismillah

--- be/index.js
+++ be/index.js
@@ -12,24 +12,24 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middleware
+
 app.use(cors({
   origin: [
-    "http://localhost:5501", // local dev
-    "https://fe-notessyifa-dot-fit-crow-450802-e0.uc.r.appspot.com", // FE deploy
-    "https://notes-syifa194-797713225706.us-central1.run.app", // backend sendiri 
+    "http://localhost:5501",
+    "https://fe-notessyifa-dot-fit-crow-450802-e0.uc.r.appspot.com",
+    "https://notes-syifa194-797713225706.us-central1.run.app",
   ],
   credentials: true,
 }));
 
+app.use(cookieParser());
+app.use(express.json());
 
-  app.use(cookieParser());
-  app.use(express.json());
+app.use(NotesRoute);
 
-  // Routes API
-  app.use(NotesRoute);
+const PORT = process.env.PORT || 3000;
 
- const PORT = process.env.PORT || 3000;
+console.log("Starting server on port:", PORT);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
