38654579ef3ffd6523956aaafb047c0348ef2cf4
unknown <syifanurramadhani54@gmail.com>
2025-05-22 15:17:36 +0700

    bismillah

--- be/models/userModel.js
+++ be/models/userModel.js
@@ -1,21 +1,36 @@
-import { Sequelize } from "sequelize"
-import db from "../config/database.js"
+import { Sequelize } from "sequelize";
+import db from "../config/database.js";
 
 const { DataTypes } = Sequelize;
 
-const User = db.define("user", {
-  id_user: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const User = db.define(
+  "user",
+  {
+    id_user: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    refresh_token: {
+      type: DataTypes.TEXT,
+    },
   },
-  username: Sequelize.STRING,
-  password: Sequelize.STRING,
-  refresh_token: Sequelize.TEXT,
-}, {
-  freezeTableName: true 
-});
+  {
+    freezeTableName: true,
+  }
+);
 
-db.sync().then(() => console.log("Database synced"));
+db.sync()
+  .then(() => console.log("✅ User table synced"))
+  .catch((err) => console.error("❌ Sync error:", err));
 
 export default User;
