d84281c88080c37292c85d708d470e14bfc0bdfc
unknown <syifanurramadhani54@gmail.com>
2025-05-22 13:33:24 +0700

    bismillah

--- be/middleware/VerifyToken.js
+++ be/middleware/VerifyToken.js
@@ -4,12 +4,11 @@ export const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  // kalau tokennya gaada
-  if (token == null) return res.sendStatus(401);
+  if (!token) return res.sendStatus(401); // Unauthorized
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-    if (error) return res.sendStatus(403);
-    req.email = decoded.email;
+    if (error) return res.sendStatus(403); // Forbidden
+    req.user = decoded; // simpan semua info user (bisa username, id_user, dll)
     next();
   });
 };
