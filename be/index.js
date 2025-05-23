ac581ceeed3e6743b4025b37fa52eb5240e9efad
unknown <syifanurramadhani54@gmail.com>
2025-05-23 11:23:39 +0700

    port

--- be/index.js
+++ be/index.js
@@ -28,7 +28,9 @@ app.use(express.json());
 app.use(NotesRoute);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
