1966e7d908097bf2fd1109f7898444a8cfd8801e
unknown <syifanurramadhani54@gmail.com>
2025-05-25 13:11:16 +0700

    bismillah

--- be/index.js
+++ be/index.js
@@ -1,21 +1,18 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
-import { fileURLToPath } from "url";
-import NotesRoute from "./routes/NotesRoute.js";
+import UserRoute from "./routes/UserRoute.js";
 import "dotenv/config";
 
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 
 app.use(cors({
   origin: [
-    "http://localhost:5501",
+    "http://localhost:8080",
     "https://fe-notessyifa-dot-fit-crow-450802-e0.uc.r.appspot.com",
     "https://notes-syifa194-797713225706.us-central1.run.app",
   ],
@@ -25,11 +22,11 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(NotesRoute);
+app.use(UserRoute);
+
 
 const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
